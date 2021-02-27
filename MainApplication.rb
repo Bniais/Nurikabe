a1897a540e23368fb943fc65839b2e5644db6bac
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-02-27 11:27:01 +0100

    Ajout du fichier MainApplication.rb

---
+++ MainApplication.rb
@@ -0,0 +1,24 @@
+class MainApplication
+    # sauveagrdes contient l'ensemble des sauvegardes (recompenses, parties, scores et parametres)
+    attr_reader :sauvegardes
+    attr_reader :mode
+    attr_reader :listeFenetre
+
+    # Methode qui permet d'initialiser le jeu
+    def initialiser()
+        #
+    end
+
+    # Methode qui permet de lancer une partie avec :
+    # * un mode donne
+    # * un difficulte donnee
+    # * une sauvegarde de partie
+    def lancerPartie(mode,difficulte,sauvegarde)
+        #
+    end
+
+    # Methode qui permet de quitter le jeu
+    def quitter()
+        #
+    end
+end

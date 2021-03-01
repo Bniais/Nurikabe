8e41c7419feff1937e166de61bd231cbdea2075e
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-01 16:55:35 +0100

    Ajout du fichier SauvegardePartie.rb

---
+++ SauvegardePartie.rb
@@ -0,0 +1,14 @@
+# Classe qui gere la sauvegarde de partie
+class SauvegardePartie
+    attr_reader :partie
+
+    # Methode qui permet de sauvegarder une partie
+    def sauvegarder(partie)
+        #
+    end
+
+    # Methode qui permet de charger une partie
+    def charger(partie)
+        #
+    end
+end

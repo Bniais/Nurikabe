3518cbb8e8bc174e468e0ca04721443cc41457f0
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-01 17:50:08 +0100

    Ajout du fichier FenetreAPropos.rb

---
+++ FenetreAPropos.rb
@@ -0,0 +1,9 @@
+load "Fenetre.rb"
+
+# Classe qui gere la fenetre 'A propos'
+class FenetreAPropos < Fenetre
+    # Methode qui permet de revenir a la fenetre precedente
+    def listenerRetourArriere()
+        #
+    end
+end

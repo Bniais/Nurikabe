2d3d4be9cddbd3219fa71cd6aac016c1ff969182
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-01 17:14:50 +0100

    Ajout du fichier FenetreMenu.rb

---
+++ FenetreMenu.rb
@@ -0,0 +1,34 @@
+load "Fenetre.rb"
+
+# Classe qui gere la fenetre du menu
+class FenetreMenu < Fenetre
+    # Methode qui renvoie le mode choisi par l'utilisateur
+    def listenerChoixMode()
+        #
+    end
+
+    # Methode qui renvoie le niveau de difficulte choisi par l'utilisateur
+    def listenerChoixDifficulte()
+        #
+    end
+
+    # Methode qui renvoie la grille choisie par l'utilisateur
+    def listenerChoixGrille()
+        #
+    end
+
+    # Methode qui permet d'ouvrir la fenetre des parametres
+    def listenerOuvrirOption()
+        #
+    end
+
+    # Methode qui permet d'ouvrir la fenetre 'A propos'
+    def listenerOuvrirAPropos()
+        #
+    end
+
+    # Methode qui permet de quitter la fenetre de menu
+    def listenerQuitter()
+        #
+    end
+end

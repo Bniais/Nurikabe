26b87af344a76c1e160b3821057b5a533e23873f
Bniais <hugo.lecomte72@hotmail.fr>
2021-03-10 11:55:18 +0100

    base de partie et coups
    Pas vraiment testé, mais y a pas d'erreur

--- Partie/Coup.rb
+++ Partie/Coup.rb
@@ -1,8 +1,13 @@
 class Coup < Partie
+  private_class_method :new
 
-    attr_accessor :case, :couleur
+  attr_accessor :case, :couleur, :couleurBase
 
-    def creer(case, int)
-      #
-    end
+  def creer(case_, couleur, couleurBase)
+    new(case_, couleur, couleurBase)
+  end
+
+  def initialize(case_, couleur, couleurBase)
+    @case, @couleur, @couleurBase = case_, couleur, couleurBase
+  end
 end

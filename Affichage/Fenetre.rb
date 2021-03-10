b6862a43e59f7e3a231e8a0dfc6caa4dc4b065f9
El Kandoussi Adnan <alzi0masta@gmail.com>
2021-03-10 11:51:24 +0100

    Update Fenetre.rb

--- Affichage/Fenetre.rb
+++ Affichage/Fenetre.rb
@@ -1,5 +1,7 @@
+require 'gtk3'
+
 # Classe abstraite qui gere l'interface
-class Fenetre
+class Fenetre 
     # titre de la fenetre
     attr_accessor :titre
     # application
@@ -8,16 +10,24 @@ class Fenetre
 
     # Methode pour creer une fenetre
     def creer()
-        #
+
+        @menu = Gtk::Window.new("Menu principal")
+        @menu.set_default_size(745,671)
+        @menu.set_resizable(false)
+        
     end
 
     # Methode qui permet d'ouvrir la fenetre
     def ouvrir()
-        #
+        @menu.show_all
     end
 
     # Methode qui permet de fermer la fenetre
     def listenerQuitter()
-        #
+
+        @menu.signal_connect('destroy'){
+            Gtk.main_quit()
+        }
+        
     end
 end

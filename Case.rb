539047dfcbb51cc0ef43b41ee58080a66ad14c30
ysendark <m.ster@outlook.fr>
2021-03-10 11:57:39 +0100

    avancer Grille et Case

--- Case.rb
+++ Case.rb
@@ -2,29 +2,41 @@ class Case
 
   attr_accessor :couleur, :positionX, :positionY
 
-  def creer(int, int, int)
+  def Case.creer( uneCouleur, posX, posY)
     #
+    @couleur = uneCouleur
+    @positionX = posX
+    @positionY = posY
+
   end
 
 
   #donne a couleur de la case
   def getCouleur()
     #return int
+    return couleur
+
   end
 
   #donne la position X
   def getPositionX()
     #return int
+    return positionX
+
   end
 
   #donne la position Y
   def getPositionY()
     #return int
+    return positionY
+
   end
 
   #change la couleur de la case
-  def setCouleur(int)
+  def setCouleur( nouvelleCouleur )
     #return void
+    couleur = nouvelleCouleur
+
   end
 
 end

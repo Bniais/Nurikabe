ebdc4a52ece43bb85bdbd2b6cd9c0ebf75ce5fa1
El Kandoussi Adnan <alzi0masta@gmail.com>
2021-03-25 19:22:34 +0100

    Ajout du texte A Propos

--- Affichage/FenetreAPropos.rb
+++ Affichage/FenetreAPropos.rb
@@ -35,6 +35,17 @@ class FenetreAPropos < Fenetre
         # VUE PRINCIPAL
         # EDIT HERE
         # ADD CONTENT HERE IN BOX
+        textBuff = Gtk::TextBuffer.new()
+        textBuff.text = "Le Nurikabe (d'un esprit du folklore japonais) est un puzzle japonais dans le style du sudoku.
+      Ce jeu, quelquefois appelé 'îlot dans le courant', est un puzzle à résolution binaire. On peut décider, pour chaque cellule, si elle est blanche ou noire en fonction de certaines règles.
+      Le Nurikabe est un monstre de la mythologie japonaise. Il apparaît la nuit, bloquant le passage aux personnes. Ces derniers temps, il est représenté comme un mur avec des jambes, mais sa réelle forme est un chien à trois yeux ayant les oreilles tombantes. "
+
+        textView = Gtk::TextView.new( textBuff )
+        textView.set_editable(false)
+        textView.set_wrap_mode(Gtk::WrapMode::WORD)
+        textView.set_width_request(700)
+        setmargin(textView,80,15,70,70)
+        box.add(textView)
 
         return box
     end

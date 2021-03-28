d709551206848a0ed087352fafbb810ddf204b6e
Timothée <timoxr2@gmail.com>
2021-03-28 19:38:13 +0200

    Dictionnaire français et anglais complet, et création des indices dans langue.rb

--- Parametres/Langue.rb
+++ Parametres/Langue.rb
@@ -1,8 +1,32 @@
 # Classe qui gere les langues
 
-TXT_PARTIE_LIBRE = 0
-TXT_CONTRE_LA_MONTRE = 1
-TXT_MODE_SURVIE = 2
+LIBRE = 1
+CONTRE-LA-MONTRE = 2
+SURVIE = 3
+TUTORIEL = 4
+CLASSEMENT = 5
+PARAMETRES = 6
+A_PROPOS = 7
+QUITTER = 8
+FACILE = 9
+MOYEN = 10
+DIFFICILE = 11
+MEILLEUR_SCORE = 12
+ACTUALISER = 13
+RETOUR = 14
+JEU = 15
+UTILISATEUR = 16
+INTERFACE = 17
+AUDIO = 18
+CASES_GRISES = 19
+COMPTEUR_D_ILOT = 20
+AFFICHER_PORTEE = 21
+MURS_2X2 = 22
+MODE_SOMBRE = 23
+CHOISIR_UNE_LANGUE = 24
+IMPORTER_UNE_LANGUE = 25
+GRILLE = 26
+PARTIE = 27
 
 class Langue
     # nom de la langue
@@ -32,3 +56,5 @@ class Langue
       return "#{@dico}"
     end
 end
+
+langue = Langue.creer("fr.txt")

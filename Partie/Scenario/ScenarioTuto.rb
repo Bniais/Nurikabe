d75ca8398e4d0b524a45e71edae837a391f04545
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-04-02 17:25:22 +0200

    création des grilles du tuto

--- Partie/Scenario/ScenarioTuto.rb
+++ Partie/Scenario/ScenarioTuto.rb
@@ -12,33 +12,28 @@ class ScenarioTuto < Scenario
     end
 
     def creerGrille()
-        # @@maPartie = Partie.creer(Grille.creer(10,
-        # [
-        #   [Case.creer(Couleur::ILE_3, 0, 0) ,Case.creer(Couleur::BLANC, 1, 0),Case.creer(Couleur::BLANC, 2, 0),Case.creer(Couleur::BLANC, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 5, 0), Case.creer(Couleur::ILE_2, 6, 0), Case.creer(Couleur::BLANC, 7, 0)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 1) ,Case.creer(Couleur::BLANC, 1, 1),Case.creer(Couleur::ILE_2, 2, 1),Case.creer(Couleur::BLANC, 3, 1), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+        grille8 = Grille.creer(11,[
+            [Case.creer(Couleur::ILE_3, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::GRIS, 2, 0),
+                Case.creer(Couleur::NOIR, 3, 0), Case.creer(Couleur::NOIR, 4, 0),Case.creer(Couleur::NOIR, 5, 0)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+            [Case.creer(Couleur::BLANC, 0, 1), Case.creer(Couleur::BLANC, 1, 1), Case.creer(Couleur::NOIR, 2, 1),
+                Case.creer(Couleur::NOIR, 3, 1), Case.creer(Couleur::ILE_1, 4, 1),Case.creer(Couleur::NOIR, 5, 1)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+            [Case.creer(Couleur::BLANC, 0, 2), Case.creer(Couleur::BLANC, 1, 2), Case.creer(Couleur::BLANC, 2, 2),
+                Case.creer(Couleur::ILE_2, 3, 2), Case.creer(Couleur::NOIR, 4, 2),Case.creer(Couleur::BLANC, 5, 2)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+            [Case.creer(Couleur::BLANC, 0, 3), Case.creer(Couleur::BLANC, 1, 3), Case.creer(Couleur::BLANC, 2, 3),
+                Case.creer(Couleur::BLANC, 3, 3), Case.creer(Couleur::BLANC, 4, 3),Case.creer(Couleur::BLANC, 5, 3)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+            [Case.creer(Couleur::ILE_1, 0, 4), Case.creer(Couleur::NOIR, 1, 4), Case.creer(Couleur::BLANC, 2, 4),
+                Case.creer(Couleur::NOIR, 3, 4), Case.creer(Couleur::ILE_1, 4, 4),Case.creer(Couleur::BLANC, 5, 4)],
 
-        #   [Case.creer(Couleur::BLANC, 0, 0) ,Case.creer(Couleur::ILE_4, 1, 0),Case.creer(Couleur::NOIR, 2, 0),Case.creer(Couleur::ILE_5, 3, 0), Case.creer(Couleur::BLANC, 4, 0),
-        #   Case.creer(Couleur::BLANC, 0, 0), Case.creer(Couleur::BLANC, 1, 0), Case.creer(Couleur::NOIR, 2, 0)],
+            [Case.creer(Couleur::NOIR, 0, 5), Case.creer(Couleur::NOIR, 1, 5), Case.creer(Couleur::ILE_2, 2, 5),
+                Case.creer(Couleur::NOIR, 3, 5), Case.creer(Couleur::BLANC, 4, 5),Case.creer(Couleur::BLANC, 5, 5)],
+        ], nil, nil)
 
-        # ]), nil, nil)
     end
 
     def verifierEtat()

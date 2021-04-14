0a290cb9f26308c0f265aa9835327176955d33b5
Sam <45427131+samymahi@users.noreply.github.com>
2021-04-14 10:49:14 +0200

    [rDoc] - Scrore
    Commentaire au format rDoc

--- Score/Score.rb
+++ Score/Score.rb
@@ -2,24 +2,21 @@ class Score
 
     attr_accessor :score, :malus, :nbAideUtilisee
 
+    ## 
+    # Methode qui permet de cree un score
     def creer(score, malus)
       @score = score
       @malus = malus
     end
 
-    #donne la valeur
+    ##
+    # score donne la valeur
     def getValeur()
       return @score
     end
 
-
-    #donne le nbombre d'aide utilisee
-    def getAideUtilisee()
-      #return int
-    end
-
-
-    #donne le nombre de malus 
+    ##
+    # donne le nombre de malus 
     def getMalus()
       return @malus
     end
